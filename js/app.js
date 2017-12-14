31cc3cd2d4a6edcec46cd8435cfef438e4b12ae3
Andres Chavez Sosa <andres.chavezsosa1494@gmail.com>
2017-12-14 13:24:17 -0600

    Se organizan tabs en carpeta con ng-include.

--- js/app.js
+++ js/app.js
@@ -16,6 +16,12 @@ app.controller('mainCtrl', ['$scope','$http','$cookies',function ($scope, $http,
 	console.log($scope.usuarioSesion)
 
 	$scope.sidebar = 'templates/sidebar.html';
+	$scope.tab_resumen = 'templates/Tabs_Templates/resumen.html';
+	$scope.tab_expediente = 'templates/Tabs_Templates/expediente.html';
+	$scope.tab_audiencia = 'templates/Tabs_Templates/audiencia.html';
+	$scope.tab_pruebas = 'templates/Tabs_Templates/pruebas.html';
+	$scope.tab_resolucion = 'templates/Tabs_Templates/resolucion.html';
+	$scope.tab_impugnaciones = 'templates/Tabs_Templates/impugnaciones.html';
 
 	if($scope.usuarioSesion != null){
 		$scope.mostrar.sideBar = true;
